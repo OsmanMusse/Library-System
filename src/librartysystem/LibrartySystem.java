cd0871a3472b368373205b67e6532e594e7d8696
mezut <you@example.com>
2018-05-03 00:33:36 +0100

    Adding comments to my java program so it can be understood.

--- src/librartysystem/LibrartySystem.java
+++ src/librartysystem/LibrartySystem.java
@@ -9,9 +9,6 @@ import java.io.File;
 import java.util.*;
 import java.io.FileInputStream;
 
-
-
-
 /**
  *
  * @author mezut
@@ -24,123 +21,138 @@ import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class LibrartySystem {
 
     /**
      * @param args the command line arguments
      */
-    
     static String fileName = null;
     static Library bookLib = new Library();
     static Scanner in = new Scanner(System.in); // Takes User Input
     static Boolean running = true;
-    
-    
-   
-    //Adding the Library Class
-    
-    
-    
-    public static void main(String[] args) throws ClassNotFoundException {
-        
-              while(running){
-     
-       System.out.println("\nEnter 0 for Loading the Library.");
-       System.out.println("\nEnter 1 to save and quit the Library.");
-       System.out.println("\nEnter 2 for Listing all the Books in the Library.");
-       System.out.println("\nEnter 3 for Adding a Book to the Library");
-       System.out.println("\nEnter one of the numbers listed above:");
-     
-         // Holds the input for the answer the user enters
-       
-         int answer = in.nextInt();
-         
-     
-       
-       //Creating A Switch Statement
-       switch(answer){
-           case 0:
-            
-          
-             System.out.println("Enter the file name to load");
-             fileName = in.next();
-             loadScript(fileName);
-             
-           break;
-           
-           case 1:
-             saveAndQuit();
-                 
-           break;
-           
-           case 2:
-             //Do the following
-               System.out.println(bookLib.toString());
-           break;
-           
-           case 3:
-             //Do the following
-               addBook();
-           break;
-                 
-       }
-         
-      } // while loop closes
-        //If while Loop value equals false do the following
-        
-        System.exit(0);
-         
-              
-    }     
 
-    private static void loadScript(String fileName) throws ClassNotFoundException {
-             FileInputStream firstInput = null;
-             ObjectInputStream in = null;
-             
-             
-             File firstFile = new File(fileName + ".ser");
-             if(firstFile.exists()){
-                     
-          
-           try {
-               firstInput = new FileInputStream(firstFile);
-               in = new ObjectInputStream(firstInput);
-                bookLib = (Library) in.readObject();
-                firstInput.close();
-                in.close();
-            } catch (FileNotFoundException ex) {
-               Logger.getLogger(LibrartySystem.class.getName()).log(Level.SEVERE, null, ex);
-               System.out.println("File not found");
-           
-            } catch (IOException ex) {
-                  Logger.getLogger(LibrartySystem.class.getName()).log(Level.SEVERE, null, ex);
-                  System.out.println("File not found");
-              }
-           } else {
-                 // do the following if the file doesn't exist
-                 System.out.println("|------------------------|");
-                 System.out.println("      File not found!     ");
-                 System.out.println("|------------------------|");
-                 
-             }
+    //Adding the Library Class
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        // This is an indefinite loop tells the program to keep on running
+        while (running) {
+
+            System.out.println(""
+                    + "+-----------------------------------------------\n"
+                    + "|           Enter 0 to load a Library           |\n"
+                    + "+-----------------------------------------------       \n");
+            System.out.println(""
+                    + "+-----------------------------------------------\n"
+                    + "|      Enter 1 to save and quit the Library    |\n"
+                    + "+-----------------------------------------------       \n");
+            System.out.println(""
+                    + "+--------------------------------------------------\n"
+                    + "|  Enter 2 for listing all the book in the Library |\n"
+                    + "+--------------------------------------------------       \n");
+            System.out.println(""
+                    + "+-----------------------------------------------\n"
+                    + "|  Enter 3 for adding a new Book to the Library |\n"
+                    + "+-----------------------------------------------       \n");
+            System.out.println(""
+                    + "+-----------------------------------------------\n"
+                    + "|  Enter 4 for finding a book in the Library    |\n"
+                    + "+-----------------------------------------------       \n");
+            System.out.println("|-------------------------------------------------------------|");
+            System.out.println("            Enter one of the following above                       ");
+            System.out.println("|-------------------------------------------------------------|");
+
+            // Holds the input for the answer the user enters
+            //Creating A Switch Statement
+            int answer = in.nextInt();
+            switch (answer) {
+
+                case 0:
+
+                    System.out.println(""
+                            + "+------------------------------------------------------\n"
+                            + "|           Enter the file name to load                |\n"
+                            + "+------------------------------------------------------       \n");
+                    fileName = in.next();
+                    loadScript(fileName);
+
+                    break;
+
+                case 1:
+                    saveAndQuit();
+
+                    break;
+
+                case 2:
+                    //Do the following
+                    System.out.println(bookLib.toString());
+                    break;
+
+                case 3:
+                    //Do the following
+                    addBook();
+                    break;
+
+            }
+
+        } // while loop closes
+        
+        //If while Loop value equals false do the following
+
+        System.exit(0);
+
     }
 
+    private static void loadScript(String fileName) throws ClassNotFoundException {
+
+        // This creates a new file name with an extension of Ser
+        // Meaning that it is a serialized object
+        File firstFile = new File(fileName + ".ser");
+        if (firstFile.exists()) {
+
+            // Try and Catch statement below Checking for a certain expression to be evaluated 
+            try {
+                ObjectInputStream ois;
+                try (FileInputStream firstInput = new FileInputStream(firstFile)) {
+                    ois = new ObjectInputStream(firstInput);
+                    bookLib = (Library) ois.readObject();
+                }
+                ois.close();
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(LibrartySystem.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("File not found");
+
+            } catch (IOException ex) {
+                Logger.getLogger(LibrartySystem.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("File not found");
+            }
+        } else {
+            // do the following if the file doesn't exist
+            System.out.println("|-------------------------------------|");
+            System.out.println("            File not found!            ");
+            System.out.println("|-------------------------------------|");
+        }
+
+    }
+
+    // This method writes the object into the file
     private static void saveAndQuit() {
-        System.out.println("Enter file name: ");
-            fileName = in.next() + ".ser";
-            running = false;
+        System.out.println("|------------------------|");
+        System.out.println("     Enter file name     ");
+        System.out.println("|------------------------|");
+
+        fileName = in.next() + ".ser";
+        running = false;
+
+        try {
             FileOutputStream fos = null;
             ObjectOutputStream out = null;
-            
-        try {
-             fos = new FileOutputStream(fileName);
-             out = new ObjectOutputStream(fos);
-             out.writeObject(bookLib);
-             fos.close();
-             out.close();
-             
-            
+            fos = new FileOutputStream(fileName);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(bookLib);
+            fos.close();
+            out.close();
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(LibrartySystem.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("File not found");
@@ -149,33 +161,47 @@ public class LibrartySystem {
         }
     }
 
+    // This method is static and allows users to add new books into the library
     private static void addBook() {
-       int isbn;
-     String title;
-     String author;
-     double price;
-     
-     System.out.println("Enter Title of Book: ");
-     title = in.next();
-     
-     System.out.println("Enter ISBN Number: ");
-     isbn = in.nextInt();
-     
-     System.out.println("Enter Price: ");
-     price = in.nextDouble();
-     
-     
-     System.out.println("Enter Author: ");
-     author = in.next();
-     
-     // Creating new instance of a book
-     Book newBook = new Book(isbn, title, author, price);
-     bookLib.addBook(newBook);
-     
-     
-     
-    }
-       
-    }
-    
+        Boolean finding = true;
+        Boolean notFound = false;
+        int isbn;
+        String title;
+        String author;
+        double price;
 
+        // Indefinite Loop this loop checks if there are any existing file 
+        // If there are any files found in will run the options avaiable
+        if (finding == true || notFound) {
+
+            // Run the program
+            System.out.println("|-------------------------------------|");
+            System.out.println("      Enter the title of the book     ");
+            System.out.println("|------------------------------------|");
+
+            title = in.next();
+
+            System.out.println("|-------------------------------------|");
+            System.out.println("      Enter the ISBN Number     ");
+            System.out.println("|------------------------------------|");
+            isbn = in.nextInt();
+
+            System.out.println("|-------------------------------------|");
+            System.out.println("      Enter the Price   ");
+            System.out.println("|------------------------------------|");
+            price = in.nextDouble();
+
+            System.out.println("|-------------------------------------|");
+            System.out.println("      Enter the Author of the book     ");
+            System.out.println("|------------------------------------|");
+            author = in.next();
+
+            // Creating new instance of a book
+            Book newBook = new Book(isbn, title, author, price);
+            bookLib.addBook(newBook);
+
+        }
+
+    }
+
+}
